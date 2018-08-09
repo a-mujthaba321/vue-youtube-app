43df1e4bb5a6975c22fc9395377ee2ca8e6759fa
Ali Mujthaba <a.mujthaba321@gmail.com>
2018-08-09 20:42:54 +0500

    fixed eslint issues

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
+
+
 <template>
 <div>
-    Hello Root
+    <SearchBar/>
 </div>
 </template>
-
 <script>
+import SearchBar from './components/SearchBar';
 export default {
-    name: 'App'
+    name: 'App',
+    components:{
+        SearchBar
+    }
 };
 </script>
